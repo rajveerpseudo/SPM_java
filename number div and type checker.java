cf6cf09cb55df6a1da68b2292d8894aa416a6dd8
rajveerpseudo <71401257+rajveerpseudo@users.noreply.github.com>
2020-09-17 01:13:49 +0530

    created
    Write a program in java to print every integer between 1 and n divisible by m . Also report whether the number that is divisible by m is even or odd.

---
+++ number div and type checker.java
@@ -0,0 +1,49 @@
+import java.util.*; 
+
+public class div { 
+
+    public static void main(String args[]) { 
+
+      Scanner sc=new Scanner(System.in); 
+
+      System.out.println("Enter limit of program"); 
+
+      int n=sc.nextInt(); 
+
+      System.out.println("Enter divisibility condition"); 
+
+      int m=sc.nextInt(); 
+
+      for(int i=1;i<n;i++) 
+
+      { 
+
+          if (i%m==0) 
+
+          { 
+
+          System.out.println(i); 
+
+      { 
+
+              if (i%2==0) 
+
+              { 
+
+              System.out.println("Even No."); 
+
+              } 
+
+              else if (i%3==0) 
+
+              { 
+
+              System.out.println("Odd No."); 
+
+              } 
+
+    } 
+
+} 
+
+}}} 
