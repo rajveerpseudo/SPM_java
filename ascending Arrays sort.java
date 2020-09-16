bd707d6684e72b91f929f2c2d1c23d9dd63cf45f
rajveerpseudo <71401257+rajveerpseudo@users.noreply.github.com>
2020-09-17 01:19:18 +0530

    createdascending
    Given three numbers A, B and C , write a program to write their value in an ascending order. For Example , if A = 12, B=10, and C = 15.

---
+++ ascending Arrays sort.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+public class sort { 
+
+    public static void main(String args[]) { 
+
+    Scanner sc= new Scanner(System.in); 
+
+     int aa[]={12,10,15}; 
+
+     Arrays.sort(aa); 
+
+System.out.println("Smallest Number= "+aa[0]); 
+
+System.out.println("Next Higher Number= "+aa[1]); 
+
+System.out.println("Highest Number= "+aa[2]); 
+
+}} 
