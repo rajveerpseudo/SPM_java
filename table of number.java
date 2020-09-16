1bb6148a6f282a68323e9a25c796217bdd12779a
rajveerpseudo <71401257+rajveerpseudo@users.noreply.github.com>
2020-09-17 01:05:45 +0530

    created code
    Write a program in java to print the table of a given number.

---
+++ table of number.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+
+public class table { 
+
+    public static void main(String args[]) { 
+
+      Scanner sc=new Scanner(System.in); 
+
+      System.out.println("Enter number"); 
+
+      int n=sc.nextInt(); 
+
+      for(int i=1;i<11;i++) 
+
+      { 
+
+          System.out.println(n+"*"+i+"="+i*n); 
+
+      } 
+
+    } 
+
+    } 
