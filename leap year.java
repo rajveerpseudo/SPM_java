3fe6f13b419d77e743a16613627c16cb546043c9
rajveerpseudo <71401257+rajveerpseudo@users.noreply.github.com>
2020-09-17 01:26:39 +0530

    created
     Write a program in java to find out whether a year (entered in 4-digit number representing it) is a leap year.

---
+++ leap year.java
@@ -0,0 +1,35 @@
+import java.util.*; 
+
+public class sort { 
+
+    public static void main(String args[]) { 
+
+    Scanner sc= new Scanner(System.in); 
+
+    System.out.println("Enter the Year"); 
+
+    int y=sc.nextInt(); 
+
+    if(y%4==0) 
+
+    { 
+
+            System.out.println("It is a Leap Year"); 
+
+    } 
+
+    else 
+
+    { 
+
+            System.out.println("It is not a Leap Year"); 
+
+ 
+
+    } 
+
+    } 
+
+} 
+
+ 
